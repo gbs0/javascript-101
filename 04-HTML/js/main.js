c9c82992acac8f3084191f2f5d51c195a204280d
gabriel schiavo <gabriel@lewagon.org>
2019-08-01 23:11:00 -0300

    remove old js scripts

--- 04-HTML/js/main.js
+++ 04-HTML/js/main.js
@@ -2,4 +2,4 @@ console.log("Hello World!");
 
 var fan = $('.vinyl')
 
-// TweenMax.to(fan, 2, {rotation:360, repeat:-1, ease:Linear.easeInOut})
+TweenMax.to(fan, 2, {rotation:360, repeat:-1, ease:Linear.easeInOut})
