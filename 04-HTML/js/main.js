a82d563787fc622faabea624e0434f3040a56bcf
gabriel schiavo <gabriel@lewagon.org>
2019-08-01 23:26:33 -0300

    add next steps

--- 04-HTML/js/main.js
+++ 04-HTML/js/main.js
@@ -3,3 +3,10 @@ console.log("Hello World!");
 var fan = document.querySelector(".vinyl");
 
 TweenMax.to(fan, 2, {rotation:360, repeat:-1, ease:Linear.easeInOut})
+
+// Código de descer
+// TweenMax.to(".box", 1, {
+//   y: function() {
+//     return Math.random() * 300;
+//   }
+// });
