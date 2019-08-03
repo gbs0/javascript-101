0d16130d6b2dfd559b8b9aa1faa4182e322133b0
gabriel schiavo <gabriel@lewagon.org>
2019-08-03 10:34:37 -0300

    add first behavior in vinyl

--- 04-HTML/js/main.js
+++ 04-HTML/js/main.js
@@ -1,8 +1,9 @@
 console.log("Hello World!");
 
-var fan = document.querySelector(".vinyl");
+let fan = document.querySelector(".vinyl");
 
-TweenMax.to(fan, 2, {rotation:360, repeat:-1, ease:Linear.easeInOut})
+// TweenMax.to(fan, 2, {rotation:360, repeat:-1, ease:Linear.easeInOut})
+
 
 // Código de descer
 // TweenMax.to(".box", 1, {
@@ -10,3 +11,21 @@ TweenMax.to(fan, 2, {rotation:360, repeat:-1, ease:Linear.easeInOut})
 //     return Math.random() * 300;
 //   }
 // });
+
+
+let isPaused = false
+  const moveVinyl = (n) => {
+  // let lamp = document.getElementById("lamp")
+  if (!isPaused){
+    if (n === 1){
+      // Stop
+			TweenMax.to(fan, 2, {rotation:360, repeat: 0, ease:Back.easeOut})
+			isPaused = true
+    } else if (n === 2){
+      lamp.src = "images/bulb-off.jpg"
+    } else if (n === 3) {
+      lamp.src = 'images/bulb-broken.jpg'
+      isPaused = true
+    }
+  }
+}
