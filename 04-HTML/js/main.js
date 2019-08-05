10efe872d776e7ecd63800e12d396bbd5641eb00
gabriel schiavo <gabriel@lewagon.org>
2019-08-05 10:09:31 -0300

    remove unused code

--- 04-HTML/js/main.js
+++ 04-HTML/js/main.js
@@ -2,29 +2,25 @@ console.log("Hello World!");
 
 let fan = document.querySelector(".vinyl");
 
-
-// Código de descer
-
-
 let isPaused = false
-  const moveVinyl = (n) => {
-  // let lamp = document.getElementById("lamp")
-  if (!isPaused){
-    if (n === 1){		// Play Vinyl
-      TweenMax.to(fan, 2, {rotation:360, repeat:-1, ease:Linear.easeInOut})
-      console.log("Playing Vinyl");
-    } else if (n === 2){		// Hover Vinyl
-      TweenMax.to(fan, 2, {rotation:360, repeat: 0, ease:Back.easeOut})
+const moveVinyl = (n) => {
+	if (!isPaused){
+	  if (n === 1){		// Play Vinyl
+	    TweenMax.to(fan, 2, {rotation:360, repeat:-1, ease:Linear.easeInOut})
+	    console.log("Playing Vinyl");
+	  } else if (n === 2){		// Hover Vinyl
+	    TweenMax.to(fan, 2, {rotation:360, repeat: 0, ease:Back.easeOut})
 			isPaused = true
-    } else if (n === 3) {		// Stop Vinyl
-      TweenMax.to(".vinyl", 1, {
+			console.log("Hover Vinyl");
+	  } else if (n === 3) {		// Stop Vinyl
+	    TweenMax.to(".vinyl", 1, {
 			  y: function() {
 			    return Math.random() * 300;
-			  }
-			});
-      isPaused = true
-    }
-  }
+			  	}
+				});
+	    isPaused = true
+	  }
+	}
 }
 
 let playButton = document.querySelectorAll("button")[0].addEventListener("click", moveVinyl(1));
