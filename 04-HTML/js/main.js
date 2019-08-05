e7944c9f5a69c48c230d60db9c7579fde76eae25
gabriel schiavo <gabriel@lewagon.org>
2019-08-05 11:42:30 -0300

    add function for click on play and pause buttons

--- 04-HTML/js/main.js
+++ 04-HTML/js/main.js
@@ -4,7 +4,6 @@ let vinyl = document.querySelector(".vinyl");
 
 let isPaused = false
 const moveVinyl = (event) => {
-	console.log(event);
 	if (!isPaused){
 	  if (event === 1){		// Play Vinyl
 	    TweenMax.to(vinyl, 2, {rotation:360, repeat:-1, ease:Linear.easeInOut})
@@ -24,6 +23,5 @@ const moveVinyl = (event) => {
 	}
 }
 
-// let playButton = document.querySelectorAll("button")[0].addEventListener("click", console.log("Click"));
 let playButton = document.querySelectorAll("button")[0].addEventListener("click", moveVinyl);
-// let stopButton = document.querySelectorAll("button")[1].addEventListener("click", moveVinyl(3));
+let stopButton = document.querySelectorAll("button")[1].addEventListener("click", moveVinyl);
