d655ce82a2bc86c2ae8bd2736772b6338b2d99d1
gabriel schiavo <gabriel@lewagon.org>
2019-08-05 10:01:04 -0300

    update code for listen btn event

--- 04-HTML/js/main.js
+++ 04-HTML/js/main.js
@@ -2,31 +2,29 @@ console.log("Hello World!");
 
 let fan = document.querySelector(".vinyl");
 
-// TweenMax.to(fan, 2, {rotation:360, repeat:-1, ease:Linear.easeInOut})
-
 
 // Código de descer
-// TweenMax.to(".box", 1, {
-//   y: function() {
-//     return Math.random() * 300;
-//   }
-// });
 
 
 let isPaused = false
   const moveVinyl = (n) => {
   // let lamp = document.getElementById("lamp")
   if (!isPaused){
-    // Play Vinyl
-    if (n === 1){
-      // Stop
-			TweenMax.to(fan, 2, {rotation:360, repeat: 0, ease:Back.easeOut})
+    if (n === 1){		// Play Vinyl
+      TweenMax.to(fan, 2, {rotation:360, repeat:-1, ease:Linear.easeInOut})
+      console.log("Playing Vinyl");
+    } else if (n === 2){		// Hover Vinyl
+      TweenMax.to(fan, 2, {rotation:360, repeat: 0, ease:Back.easeOut})
 			isPaused = true
-    } else if (n === 2){
-      lamp.src = "images/bulb-off.jpg"
-    } else if (n === 3) {
-      lamp.src = 'images/bulb-broken.jpg'
+    } else if (n === 3) {		// Stop Vinyl
+      TweenMax.to(".vinyl", 1, {
+			  y: function() {
+			    return Math.random() * 300;
+			  }
+			});
       isPaused = true
     }
   }
 }
+
+let playButton = document.querySelectorAll("button")[0].addEventListener("click", moveVinyl(1));
