47c0de78e98ddd9655e44fe7173f53da4de104a3
gabriel schiavo <gabriel@lewagon.org>
2019-08-06 19:48:28 -0300

    add logic for timeine display

--- 04-HTML/js/main.js
+++ 04-HTML/js/main.js
@@ -1,25 +1,49 @@
-const vinyl = document.querySelector(".vinyl");
+const tl = new TimelineMax({paused:true});
 
-let isPaused = false
-const moveVinyl = (event) => {
-	if (!isPaused){
-	  if (event === 1){		// Play Vinyl
-	    TweenMax.to(vinyl, 2, {rotation:360, repeat:-1, ease:Linear.easeInOut})
-	    console.log("Playing Vinyl");
-	  } else if (event === 2){		// Hover Vinyl
-	    TweenMax.to(vinyl, 2, {rotation:360, repeat: 0, ease:Back.easeOut})
-			isPaused = true
-			console.log("Hover Vinyl");
-	  } else if (event === 3) {		// Stop Vinyl
-	    TweenMax.to(".vinyl", 1, {
-			  y: function() {
-			    return Math.random() * 300;
-			  	}
-				});
-	    isPaused = true
-	  }
-	}
+const vinyl = document.querySelector(".vinyl");
+const playButton = document.querySelectorAll("button")[0];
+const reverseButton = document.querySelectorAll("button")[1];
+const stopButton = document.querySelectorAll("button")[2];
+
+tl.to(vinyl, 1, {rotation:360, repeat:-1, ease:Linear.easeInOut}); 
+
+let IsPaused = false
+
+playButton.onclick = function() {
+  tl.play();
+  console.log("Playing vinyl");
+  // TweenMax.to(vinyl, 2, {rotation:360, repeat:-1, ease:Linear.easeInOut});
 }
 
-let playButton = document.querySelectorAll("button")[0].addEventListener("click", moveVinyl);
-let stopButton = document.querySelectorAll("button")[1].addEventListener("click", moveVinyl);
+stopButton.onclick = function() {
+  tl.pause();
+	// Stop playing vinil
+}
+
+reverseButton.onclick = function() {
+	tl.reverse();
+	// TweenMax.to(vinyl, 2, {rotation:-1, repeat:-1, ease:Linear.easeInOut});
+}
+
+// let isPaused = false
+// const moveVinyl = (event) => {
+// 	if (!isPaused){
+// 	  if (event === 1){		// Play Vinyl
+// 	    TweenMax.to(vinyl, 2, {rotation:360, repeat:-1, ease:Linear.easeInOut});
+// 	    console.log("Playing Vinyl");
+// 	  } else if (event === 2){		// Hover Vinyl
+// 	    TweenMax.to(vinyl, 2, {rotation:360, repeat: 0, ease:Back.easeOut});
+// 			console.log("Hover Vinyl");
+// 	  } else if (event === 3) {		// Stop Vinyl
+// 	    TweenMax.to(".vinyl", 1, {
+// 			  y: function() {
+// 			    return Math.random() * 300;
+// 			  	}
+// 				});
+// 	    isPaused = true
+// 	  }
+// 	}
+// }
+
+// const playButton = document.querySelectorAll("button")[0].addEventListener("click", moveVinyl);
+// const stopButton = document.querySelectorAll("button")[1].addEventListener("click", moveVinyl);
