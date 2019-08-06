abd9b0b8248613834769de825ec9435dd491bb4b
gabriel schiavo <gabriel@lewagon.org>
2019-08-06 19:55:22 -0300

    finish timeline code

--- 04-HTML/js/main.js
+++ 04-HTML/js/main.js
@@ -7,8 +7,6 @@ const stopButton = document.querySelectorAll("button")[2];
 
 tl.to(vinyl, 1, {rotation:360, repeat:-1, ease:Linear.easeInOut}); 
 
-let IsPaused = false
-
 playButton.onclick = function() {
   tl.play();
   console.log("Playing vinyl");
@@ -23,26 +21,3 @@ reverseButton.onclick = function() {
 	tl.reverse();
 	console.log("Reverse vinyl");
 }
-
-// let isPaused = false
-// const moveVinyl = (event) => {
-// 	if (!isPaused){
-// 	  if (event === 1){		// Play Vinyl
-// 	    TweenMax.to(vinyl, 2, {rotation:360, repeat:-1, ease:Linear.easeInOut});
-// 	    console.log("Playing Vinyl");
-// 	  } else if (event === 2){		// Hover Vinyl
-// 	    TweenMax.to(vinyl, 2, {rotation:360, repeat: 0, ease:Back.easeOut});
-// 			console.log("Hover Vinyl");
-// 	  } else if (event === 3) {		// Stop Vinyl
-// 	    TweenMax.to(".vinyl", 1, {
-// 			  y: function() {
-// 			    return Math.random() * 300;
-// 			  	}
-// 				});
-// 	    isPaused = true
-// 	  }
-// 	}
-// }
-
-// const playButton = document.querySelectorAll("button")[0].addEventListener("click", moveVinyl);
-// const stopButton = document.querySelectorAll("button")[1].addEventListener("click", moveVinyl);
