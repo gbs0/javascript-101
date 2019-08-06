5de68b58043aa0cd6bf1d98c06aeca421ea00955
gabriel schiavo <gabriel@lewagon.org>
2019-08-06 16:51:58 -0300

    remove isPaused boolean

--- 04-HTML/js/main.js
+++ 04-HTML/js/main.js
@@ -4,11 +4,10 @@ let isPaused = false
 const moveVinyl = (event) => {
 	if (!isPaused){
 	  if (event === 1){		// Play Vinyl
-	    TweenMax.to(vinyl, 2, {rotation:360, repeat:-1, ease:Linear.easeInOut})
+	    TweenMax.to(vinyl, 2, {rotation:360, repeat:-1, ease:Linear.easeInOut});
 	    console.log("Playing Vinyl");
 	  } else if (event === 2){		// Hover Vinyl
-	    TweenMax.to(vinyl, 2, {rotation:360, repeat: 0, ease:Back.easeOut})
-			isPaused = true
+	    TweenMax.to(vinyl, 2, {rotation:360, repeat: 0, ease:Back.easeOut});
 			console.log("Hover Vinyl");
 	  } else if (event === 3) {		// Stop Vinyl
 	    TweenMax.to(".vinyl", 1, {
@@ -21,5 +20,5 @@ const moveVinyl = (event) => {
 	}
 }
 
-let playButton = document.querySelectorAll("button")[0].addEventListener("click", moveVinyl);
-let stopButton = document.querySelectorAll("button")[1].addEventListener("click", moveVinyl);
+let playButton = document.querySelectorAll("button")[0].bind("onclick", moveVinyl);
+let stopButton = document.querySelectorAll("button")[1].bind("onclick", moveVinyl);
