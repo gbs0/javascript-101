9f097e3696e9366cd29856130af65101058d6f6e
gabriel schiavo <gabrielschiavo0@gmail.com>
2019-12-13 11:07:35 -0300

    add right syntax for calculator script

--- 03-loops-and-functions/02-calculator.js
+++ 03-loops-and-functions/02-calculator.js
@@ -1,17 +1,17 @@
 // Code a calculator
-const sum = (n1, n2) { // const sum = (a, b) => a + b
+const sum = (n1, n2) => { // const sum = (a, b) => a + b
 	return n1 + n2
 }
 
-const multiply = (n1, n2) {  // const multiply = (a, b) => a * b
+const multiply = (n1, n2) => {  // const multiply = (a, b) => a * b
 	return n1 * n2
 }
 
-const subtraction = (n1, n2) { // const sub = (a, b) => a - b
+const subtraction = (n1, n2) => { // const sub = (a, b) => a - b
 	return n1 - n2	
 }
 
-const division = (n1, n2) {  // const div(a, b) => a / b
+const division = (n1, n2) => {  // const div(a, b) => a / b
 	return n1 / n2
 }
 
--- 03-loops-and-functions/__tests__/02-calculator.test.js
+++ 03-loops-and-functions/__tests__/02-calculator.test.js
@@ -1,2 +1,14 @@
 const { sum, multiply, subtraction, division } = require('../02-calculator')
 
+test('Adding 1 + 1 equals 2', () => {
+  expect(sum(1, 1)).toBe(2)
+})
+test('Multiplying 1 * 1 equals 1', () => {
+  expect(multiply(1, 1)).toBe(1)
+})
+test('Subtracting 1 - 1 equals 0', () => {
+  expect(subtraction(1, 1)).toBe(0)
+})
+test('Dividing 1 / 1 equals 1', () => {
+  expect(division(1, 1)).toBe(1)
+})
