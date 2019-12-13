9b9c3c7e692e4c16d80a21f4199527e5971b214e
gabriel schiavo <gabrielschiavo0@gmail.com>
2019-12-13 10:20:50 -0300

    add another test for weather functions

--- 02-boolean-algebra/__tests__/logical_operators.test.js
+++ 02-boolean-algebra/__tests__/logical_operators.test.js
@@ -1,20 +1,33 @@
 const weather = require('../logical_operators');
 
-// describe('#weather should return correct responses for', () => {
-//   const rain = true;
-//   const wind = true;
+describe('#weather should return correct responses for', () => {
+  const rain = true;
+  const wind = true;
   
 
-//   it('Rain and Wind equals to true', () => {
-// 	  expect(weather([rain, wind])).toBe("Really stay at home!");
-//   });
+  it('Rain and Wind equals to true', () => {
+	  expect(weather([rain, wind])).toBe("Really stay at home!");
+  });
   
-// });
+});
 
-// describe('#weather should return correct responses for', () => {
-// 	const day = "Saturday";
+describe('#weather should return correct responses for', () => {
+	const day = "Saturday";
   
-//   it('It says when there are weekends', () => {
-// 	  expect(weather("Saturday")).toBe("It's Weekend!");
-//   });
-// });
+  it('It says when there are weekends', () => {
+	  expect(weather("Saturday")).toBe("It's Weekend!");
+  });
+});
+
+
+describe('Matching weather status', () => {
+  // Applies only to tests in this describe block
+  beforeEach(() => {
+    const rain = true;
+    const wind = true;
+  });
+
+  test('Test if rain and wind weather return the right result', () => {
+    expect(weather([rain, wind])).toBe("Really stay at home!");
+  });
+});
