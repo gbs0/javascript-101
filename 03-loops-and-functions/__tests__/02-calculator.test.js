dff866b59f6f18346e741064307bbc9807aa61f9
gabriel schiavo <gabrielschiavo0@gmail.com>
2019-12-13 11:14:15 -0300

    add describe in tests

--- 03-loops-and-functions/__tests__/02-calculator.test.js
+++ 03-loops-and-functions/__tests__/02-calculator.test.js
@@ -1,14 +1,29 @@
 const { sum, multiply, subtraction, division } = require('../02-calculator')
+describe('Test sum ➕ function', () => {
+  // Applies only to tests in this describe block
+  test('Adding 1 + 1 equals 2', () => {
+  	expect(sum(1, 1)).toBe(2);
+	})
+});
 
-test('Adding 1 + 1 equals 2', () => {
-  expect(sum(1, 1)).toBe(2)
-})
-test('Multiplying 1 * 1 equals 1', () => {
-  expect(multiply(1, 1)).toBe(1)
-})
-test('Subtracting 1 - 1 equals 0', () => {
-  expect(subtraction(1, 1)).toBe(0)
-})
-test('Dividing 1 / 1 equals 1', () => {
-  expect(division(1, 1)).toBe(1)
-})
+describe('Test Multiply ✖️ function', () => {
+  // Applies only to tests in this describe block
+  test('Multiplying 1 * 1 equals 1', () => {
+  	expect(multiply(1, 1)).toBe(1);
+	})
+});
+
+describe('Test Subtraction ➖ function', () => {
+  // Applies only to tests in this describe block
+  test('Subtracting 1 - 1 equals 0', () => {
+  	expect(subtraction(1, 1)).toBe(0);
+	})
+});
+
+describe('Test Divsion ➗ function', () => {
+  // Applies only to tests in this describe block
+  test('Dividing 1 / 1 equals 1', () => {
+  	expect(division(1, 1)).toBe(1);
+	})
+});
+
